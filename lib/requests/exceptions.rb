443aea2b1867bf454f2f03d88ff4e4aac3371066
Ian Cordasco <graffatcolmingov@gmail.com>
2013-02-26 18:39:26 -0500

    Create a preliminary (untested) CookieJar
    [ci skip]

--- lib/requests/exceptions.rb
+++ lib/requests/exceptions.rb
@@ -2,4 +2,5 @@ module Requests
   class MissingScheme < StandardError ; end
   class InvalidURL < StandardError ; end
   class NotImplementedYet < StandardError ; end
+  class ValueError < StandardError ; end
 end
--- lib/requests/structs.rb
+++ lib/requests/structs.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'cgi'
+require 'requests/exceptions'
 
 
 module  Requests
@@ -10,6 +12,26 @@ module  Requests
       else
         initialize_http_header(initial_headers)
       end
+    end
+  end
+
+  class CookieJar
+    include Net::HTTPHeader
+    def initialize(cookies=nil)
+      if cookies.respond_to? :each
+        cookies.each { |c| add_field(c.name, c) }
+      else
+        initialize_http_header(nil)
+      end
+    end
+
+    def add_cookie(cookie, name=nil)
+      if (not cookie.is_a? CGI::Cookie or cookie.nil?) and name.nil?
+        raise ValueError "Must provide a cookie"
+      end
+
+      name ||= cookie.name
+      add_field(name, cookie)
     end
   end
 end
